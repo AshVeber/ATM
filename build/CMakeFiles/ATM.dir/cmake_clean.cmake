8993e313f7c05fde1eb2b27e39e9f014b1486ccb
AshVeber <ashveber42@gmail.com>
2026-06-20 15:31:28 +0300

    added CMake

---
+++ build/CMakeFiles/ATM.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ATM.dir/link.d"
+  "ATM"
+  "ATM.pdb"
+  "CMakeFiles/ATM.dir/src/ATM.cpp.o"
+  "CMakeFiles/ATM.dir/src/ATM.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ATM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/ATM.cpp
+++ src/ATM.cpp
@@ -3,7 +3,26 @@
 #include <string>
 #include <fstream>
 
-#include "settings.h"
+bool isNumber(const std::string& s) {
+    if(s.empty()) {
+        return false;
+    }
+    for(char const &c : s) {
+        if(!(isdigit(c))) {
+            return false;
+        }
+    }
+    return true;
+}
+int loadPin(std::string log) {
+    std::ifstream file(log + "_pin.txt");
+    int pin;
+
+    if(file >> pin) {
+        return pin;
+    }
+    return -1;
+}
 
 std::string login() {
     std::cout << "Enter your username: " << std::endl;
